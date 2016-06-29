a2803c00513d3fa10e08d27f0e4430e06e9f9b44
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-06-29 11:52:42 +0530

    day 4

--- src/client/app/common/components/header.js
+++ src/client/app/common/components/header.js
@@ -3,19 +3,31 @@ import React, {PropTypes} from 'react';
 
 class NavIndexLink extends React.Component {
   render() {
-    
+    const isActive = this.context.router.isActive(this.props.to, true);
+
+    // eslint-disable-next-line no-ternary
+    let liClass = isActive ? 'active' : '';
+
+    return (
+      <li role="presentation" className={liClass}>
+        <IndexLink to={this.props.to}>{this.props.linkTitle}</IndexLink>
+      </li>
+    );
   }
 }
 
 NavIndexLink.contextTypes = {router: PropTypes.object};
 
+NavIndexLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  linkTitle: PropTypes.string.isRequired
+};
+
 class Header extends React.Component {
   render() {
     return (
       <ul className="header nav nav-tabs">
-        <li role="presentation">
-          <IndexLink to="/" activeClassName="active">Home</IndexLink>
-        </li>
+        <NavIndexLink to="/" linkTitle="Home" />
         <li role="presentation">
           <Link to="/todo" activeClassName="active">TODO</Link>
         </li>
