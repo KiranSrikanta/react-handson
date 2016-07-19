38a1885fbbf6b94d11384973f3297a44fed36165
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-07-19 08:02:16 +0530

    day 7

--- src/client/app/todo/components/todo-list-page.js
+++ src/client/app/todo/components/todo-list-page.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import TodoList from './todo-list';
 
 class TodoListPage extends React.Component {
+  constructor() {
+    super();
+
+    this.onComplete = this.onComplete.bind(this);
+    this.state = {
+      todos: [
+        {
+          title: 'task1',
+          description: 'important task',
+          completed: false,
+          date: new Date()
+        },
+        {
+          title: 'task2',
+          description: 'another important task',
+          completed: false,
+          date: new Date()
+        }
+      ]
+    };
+  }
+
+  onComplete(isComplete, index) {
+    const todos = this.state.todos;
+
+    todos[index].completed = isComplete;
+    this.setState({ todos });
+  }
+
   render() {
     return (
       <div className="todo-list-page">
-        Todo list goes here!
+        <TodoList todos={this.state.todos} onComplete={this.onComplete}></TodoList>
       </div>
     );
   }
--- src/client/app/todo/components/todo-list.js
+++ src/client/app/todo/components/todo-list.js
@@ -1,10 +1,52 @@
 import React, {PropTypes} from 'react';
 
 class TodoList extends React.Component {
+  constructor() {
+    super();
+
+    this.renderTodoRow = this.renderTodoRow.bind(this);
+  }
+
+  onTodoComplete(index, event) {
+    this.props.onComplete(event.target.checked, index);
+  }
+
+  renderTodoRow(todo, index) {
+    return (
+      <tr key={index}>
+        <td>{todo.title}</td>
+        <td>{todo.description}</td>
+        <td>{todo.date.toLocaleString()}</td>
+        <td><input type="checkbox" value={todo.checked} onChange={this.onTodoComplete.bind(this, index)}></input></td>
+      </tr>
+    );
+  }
+
   render() {
+    let todoListContent = null;
+    const emptyTodoListCount = 0;
+
+    if (this.props.todos.length === emptyTodoListCount) {
+      todoListContent = <div className="alert alert-warning">No todos.</div>;
+    } else {
+      todoListContent =
+        <table className="table">
+          <thead>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Date</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {this.props.todos.map(this.renderTodoRow)}
+          </tbody>
+        </table>;
+    }
+
     return (
       <div className="todo-list">
-        Todo list goes here
+      <h1>Todo List</h1>
+        {todoListContent}
       </div>
     );
   }
